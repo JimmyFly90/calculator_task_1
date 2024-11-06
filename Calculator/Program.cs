30159d1c6668a79a98270820d8e485610026dc34
James Flynn <jamesdeclonflynn@hotmail.com>
2024-11-06 15:50:08 +0000

    Latest changes dealing with exceptions caused by user input

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,11 +4,63 @@ public class Calculator
 {
     public static void Main(string[] args)
     {
-        Console.Write("Enter an operation (+, -, /, *): ");
+        char opr;
+        float first, second;
 
-        // Write code below this line
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter the first number: ");
+                first = float.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("The first number is invalid. ");
+            }
+        }
+        
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter an operation (+, -, /, *): ");
+                opr = Console.ReadLine()[0];
 
-        // Write code above this line
+                // 'Contains' method checks if a string contains a character or a substring.
+                if ("/-+*".Contains(opr))
+                    break;
+
+            }
+            catch { }
+            Console.WriteLine("The operator is invalid.");
+        }
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter the second number: ");
+                second = float.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("The second number is invalid. ");
+            }
+        }
+
+
+        float result = 0;
+        if (opr == '+')
+            result = first + second;
+        else if (opr == '-')
+            result = first - second;
+        else if (opr == '/')
+            result = first / second;
+        else if (opr == '*')
+            result = first * second;
 
         Console.WriteLine($"The result of '{first} {opr} {second}' is: {result}");
     }
